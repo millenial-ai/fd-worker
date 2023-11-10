a83d1d94b43d95fb8bf14c35f2b4e9d8206148c1
Ubuntu <ubuntu@ip-10-0-13-187.ec2.internal>
2023-11-10 15:23:04 +0000

    change start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,10 +1,7 @@
-export RCF_ENDPOINT=custom-nopca-datacamp-rcf
-export XGB_ENDPOINT=custom-nopca-datacamp-xgbsmote
-export SQS_NAME=FraudRequestQueue
 aws configure set region $(aws configure list | grep region | awk '{print $2}')
 
 cd ~/fd-worker
 
 echo "Starting app"
 
-nohup python3 main.py > /dev/null 2>&1 &
+bash "run-${env:-production}.sh"
